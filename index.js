fa5454ed120ada13712e023739d01da7a4b79504
Simon <oleurud@gmail.com>
2017-11-21 00:22:15 +0100

    s6

--- index.js
+++ index.js
@@ -9,9 +9,24 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 
+// custom auth middleware (not real ;))
+app.use(function (req, res, next) {
+    const token = req.get('Authorization')
+    console.log(token)
+
+    if (token === 'tokenmolon') {
+        next()
+    } else {
+        next(new Error('Unauthorized'))
+    }
+})
+
+
+// routes
 app.post('/test', function (req, res) {
     res.send(req.body)
 })
+
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
