a9929af19e5c66bf8612beb12cafdba03abc4840
Simon <oleurud@gmail.com>
2017-11-23 21:13:13 +0100

    s3

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-})
+app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get('/example', function (req, res) {
+app.get('/example', (req, res) => {
     res.send('GET request!')
 })
 
-app.post('/example', function (req, res) {
+app.post('/example', (req, res) => {
     res.send('POST request!')
 })
 
-app.put('/example', function (req, res) {
+app.put('/example', (req, res) => {
     res.send('PUT request!')
 })
 
-app.delete('/example', function (req, res) {
+app.delete('/example', (req, res) => {
     res.send('DELETE request!')
 })
 
-app.get('/users/:userId/books/:bookId', function (req, res) {
+app.get('/users/:userId/books/:bookId', (req, res) => {
     res.send(req.params)
 })
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
-})
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
