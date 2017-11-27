3ea8851e83296177a6ad6a45375e291bf6b72cfe
Simon <oleurud@gmail.com>
2017-11-28 00:00:26 +0100

    s19

--- src/app/middlewares/response.js
+++ src/app/middlewares/response.js
@@ -1,6 +1,10 @@
 
 module.exports = app => {
     app.use((req, res, next) => {
+        if(!res.locals.response) {
+            res.status(404).send("Route not found!")
+        }
+
         res.json({
             status: true,
             data: res.locals.response
