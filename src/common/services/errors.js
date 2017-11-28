94cd3f9667e8910ea14c873121716003d4218879
Simon <oleurud@gmail.com>
2017-11-28 21:33:07 +0100

    s20

--- src/common/services/errors.js
+++ src/common/services/errors.js
@@ -3,5 +3,9 @@ const CustomError = require('./CustomError')
 module.exports = {
     NotFound(message = 'Not found', status = 404) {
         return new CustomError(message, status)
-    }
+    },
+
+    Error500(message = 'Something was wrong', status = 500) {
+        return new CustomError(message, status)
+    },
 } 
