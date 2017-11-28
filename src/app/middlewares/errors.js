bf9deb4ecb522f20e16fd40d93abc9e90c2df5e0
Simon <oleurud@gmail.com>
2017-11-28 22:16:41 +0100

    s20

--- src/app/middlewares/errors.js
+++ src/app/middlewares/errors.js
@@ -1,9 +1,11 @@
 const CustomError = require('../../common/services/CustomError')
 const errors = require('../../common/services/errors')
+const debug = require('debug')('app:errors')
 
 module.exports = app => {
     app.use((err, req, res, next) => {
         if (!(err instanceof CustomError)) {
+            debug(err)
             err = errors.Error500()
         }
 
